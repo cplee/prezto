2040150127e3c9daf8a4c0d265545e2f7111fe09
Casey Lee <casey.lee@stelligent.com>
2016-01-12 10:43:12 -0800

    styled and dialed

--- link.zsh
+++ link.zsh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 
 setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(N); do
+  if [[ ! -a "${ZDOTDIR:-$HOME}/.${rcfile:t}" ]]; then
+    echo ">> ${rcfile}"
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  fi
 done
